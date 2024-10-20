efa08b94bc197c2b84b5c140b9cd98d2485621a9
david-pessoa <pessoa.dvlbp@gmail.com>
2024-10-20 17:02:02 -0300

    faz o bot dar resposta pronta

--- src/Chat.js
+++ src/Chat.js
@@ -6,18 +6,12 @@ import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 //const API_KEY = "sk-Ws3XgDdqZxa-sIfoih_G9WQpjr8GisDfodDOTQwRaVT3BlbkFJRPoOWUIbR72UDhNYCcKAUK-nnPksadcxq018O9v3MA"; //SamsAI
-const API_KEY = "sk-URFSgdeBHnoqKDr-IqhREP1gL7MydtkQgY3Vp1Fd44T3BlbkFJFRMgJS_ZOXxi4X_ixfedYrz3QP3Z0vMAz2zf73L2sA"; //Teste2
+const API_KEY = "sk-qA5UKrsytzH8A8NjBi3te-gKfljSacPpyyK4h_fC0AT3BlbkFJTYHUr4vFiGZzm_Jf_wbIE1AUYfjOXTzbnPk7pg1kgA"; //Teste2
 
 function Chat() {
     const [isVisible, setIsVisible] = useState(true);
 
-    const [messages, setMessages] = useState([
-        {
-            message: "Olá! Testando...",
-            user: "ChatGPT",
-            direction: "incoming",
-        }
-    ])
+    const [messages, setMessages] = useState([]) //Lista de mensagens vazia
 
     const handleSend = async (message) => {
         setIsVisible(false);
@@ -37,51 +31,12 @@ function Chat() {
     }
 
     async function processMessageToSamsAI(chatMessages){
-        //chatMessages { sender: "user" or "SamsAI", message: "Message Content..."}
-        //apiMessages { role: "user" or "assistant", content: "Message Content..."}
-
-        let apiMessages = chatMessages.map((messageObject) => {
-            let role = "";
-            if(messageObject.sender === "ChatGPT") {
-                role = "assistant"
-            }
-            else
-            {
-                role = "user"
-            }
-            return { role: role, content: messageObject.message }
-        });
-
-        const systemMessage = {
-            role: "system",
-            content: "Fale comigo como se eu fosse um advogado" //Usar para os filtros
-        }
-
-        const apiRequestBody = {
-            "model": "gpt-3.5-turbo",
-            "messages": [
-                systemMessage,
-                ...apiMessages],
-        }
-
-        await fetch("https://api.openai.com/v1/chat/completions", {method: "POST", 
-            headers: {
-                "Authorization": "Bearer " + API_KEY,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(apiRequestBody)
-        }).then((data) => {
-            return data.json();
-        }).then((data) => {
-            console.log(data)
-            console.log(data.choices[0].message.content) //E assim vai até achar o conteúdo
             setMessages(
                 [...chatMessages, {
-                    message: data.choices[0].message.content, //Coloca onde estiver o conteúdo
-                    sender: "ChatGPT",
+                    message: "Olá, como vai?", //Coloca onde estiver o conteúdo
+                    direction: "incoming",
                 }]
             )
-        });
     }
 
     const containerStyle = {
