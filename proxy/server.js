d7be7163b5487e6bfbc393f55837104716c61429
david-pessoa <pessoa.dvlbp@gmail.com>
2024-10-30 21:28:44 -0300

    tenta corrigir a porcaria do caminho

--- proxy/server.js
+++ proxy/server.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
     const proxy = createProxyMiddleware({
         target: 'https://api.langflow.astra.datastax.com',
         pathRewrite: {
-            '^/api': '/lf/efde00ae-4d32-4471-8e8d-26482560f5a9/api/v1/run/789d10ee-9573-4333-8fe2-52c048315d3d',
+            '^/api': '',
         },
         changeOrigin: true,
         secure: false,
