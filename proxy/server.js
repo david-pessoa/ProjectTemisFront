d1554ed7c954b14a57244381cd6881d66bd838c6
david-pessoa <pessoa.dvlbp@gmail.com>
2024-10-30 21:24:13 -0300

    corrige caminho

--- proxy/server.js
+++ proxy/server.js
@@ -4,12 +4,13 @@ export default function handler(req, res) {
     const proxy = createProxyMiddleware({
         target: 'https://api.langflow.astra.datastax.com',
         pathRewrite: {
-            '^api': '/lf/efde00ae-4d32-4471-8e8d-26482560f5a9/api/v1/run/789d10ee-9573-4333-8fe2-52c048315d3d',
+            '^/api': '/lf/efde00ae-4d32-4471-8e8d-26482560f5a9/api/v1/run/789d10ee-9573-4333-8fe2-52c048315d3d',
         },
         changeOrigin: true,
         secure: false,
         onProxyReq: (proxyReq, req) => {
-          proxyReq.setHeader('Authorization', `Bearer ${process.env.NEXT_PUBLIC_APPLICATION_TOKEN}`);
+          proxyReq.setHeader('Authorization', "Bearer AstraCS:eITJWgcxrHBmiMyuOBnJaQxw:f41642d1db696db910f69a8e3cc1753a69c51e2bcaa285f4e0e3805f2980dcc1");
+          proxyReq.setHeader('Content-Type', "application/json");
         },
         onProxyRes: (proxyRes, req, res) => {
             // Define cabeçalhos de CORS na resposta do proxy
