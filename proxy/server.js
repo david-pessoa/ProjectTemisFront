5d238060d64fa0afb14a334a92b5fa7f8b14ab39
david-pessoa <pessoa.dvlbp@gmail.com>
2024-10-31 21:48:43 -0300

    tenta no docker

--- proxy/server.js
+++ proxy/server.js
@@ -1,24 +1,33 @@
+import express from 'express';
+import cors from 'cors';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-export default async function handler(req, res) {
-    const proxy = createProxyMiddleware({
-        target: 'https://api.langflow.astra.datastax.com',
-        pathRewrite: {
-            '^proxy/server': '/lf/efde00ae-4d32-4471-8e8d-26482560f5a9/api/v1/run/789d10ee-9573-4333-8fe2-52c048315d3d',
-        },
-        changeOrigin: true,
-        secure: false,
-        onProxyReq: (proxyReq, req) => {
-          proxyReq.setHeader('Authorization', "Bearer AstraCS:eITJWgcxrHBmiMyuOBnJaQxw:f41642d1db696db910f69a8e3cc1753a69c51e2bcaa285f4e0e3805f2980dcc1");
-          proxyReq.setHeader('Content-Type', "application/json");
-        },
-        onProxyRes: (proxyRes, req, res) => {
-            // Define cabeçalhos de CORS na resposta do prox
-            res.setHeader('Access-Control-Allow-Origin', '*');  // Permite qualquer origem
-            res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS'); // Métodos permitidos
-            res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeçalhos permitidos
-        },
-    });
+const app = express();
+const PORT = process.env.PORT || 5500;
 
-    return proxy(req, res);
-}
+app.use(cors());
+
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: 'https://api.langflow.astra.datastax.com',
+    pathRewrite: {
+      '^/api': '/lf/efde00ae-4d32-4471-8e8d-26482560f5a9/api/v1/run/789d10ee-9573-4333-8fe2-52c048315d3d',
+    },
+    secure: false,
+    changeOrigin: true,
+    onProxyReq: (proxyReq, req) => {
+      // Filtra cabeçalhos desnecessários
+      const headersToKeep = ["Authorization", "Content-Type"];
+      headersToKeep.forEach(header => {
+        if (req.headers[header.toLowerCase()]) {
+          proxyReq.setHeader(header, req.headers[header.toLowerCase()]);
+        }
+      });
+    },
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
